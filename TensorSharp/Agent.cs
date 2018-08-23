a8e773259e197807ae8cdb7ef786e625ad1cdd72
Lee Tae Hwa <driversivese@gamil.com>
2018-08-23 14:25:38 +0900

    source code commit

---
+++ TensorSharp/Agent.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TensorSharp
+{
+    class Agent
+    {
+        static readonly float dicountFactor = 0.99f;
+
+        private GridEnvironment environment;
+        private float[,] valueTable;
+        private float[,,] policyTable;
+
+        public Agent(GridEnvironment environment)
+        {
+            this.environment = environment;
+
+            var envSize = GridEnvironment.gridSize;
+
+            valueTable = new float[envSize, envSize];
+            Array.Clear(valueTable, 0, valueTable.Length);
+
+            policyTable = new float[envSize, envSize, 4];
+            
+            for(int i = 0; i < envSize * envSize; i++)
+                for(int k=0;k<4;k++)
+                    policyTable[i/4, i%4, k] = 0.25f;
+        }
+
+        public void PolicyEvaluation()
+        {
+            var envSize = GridEnvironment.gridSize;
+
+            float[,] nextValue = new float[envSize, envSize];
+            Array.Clear(nextValue, 0, nextValue.Length);
+
+            var allState = environment.GetAllState();
+
+            foreach (var state in allState)
+            {
+                var value = 0.0;
+
+                /*
+                if(state==[2, 2])
+                {
+                    nextValue[state[0], state[1]] = 0.0f;
+                    continue;
+                }
+                */
+                
+            }
+
+        }
+
+        public void PolicyImprovement()
+        {
+
+        }
+
+        public int GetAction()
+        {
+            int idx = 0;
+
+            return idx;
+        }
+
+        public float GetPolicy()
+        {
+            float state = 0.1f ;
+
+            return state;
+        }
+
+        public float GetValue()
+        {
+            return valueTable[0, 0];
+        }
+    }
+}
---
+++ TensorSharp/GridEnvironment.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TensorSharp
+{
+    class GridEnvironment
+    {
+        //Grid information
+        public static readonly int GOLD = 1;
+        public static readonly int TRAP = -1;
+        public static readonly int PLAIN = 0;
+        public static readonly int gridSize = 5;
+
+        private int[,] grid;
+
+        public GridEnvironment()
+        {
+            //grid array initializing
+            grid = new int[gridSize, gridSize];
+            Array.Clear(grid, PLAIN, grid.Length);
+
+            //Set trap, gold position;
+            grid[1, 2] = grid[2, 1] = -1;
+            grid[2, 2] = 1;
+            
+        }
+        
+        public List<float> GetAllState()
+        {
+            List<float> stateList = new List<float>();
+
+            return stateList;
+        }
+
+        private void Set2DArray<T>(T[][] arr, int size)
+        {
+            arr = new T[size][];
+
+            for (int i = 0; i < size; i++)
+                arr[i] = new T[size];
+        }
+    }
+}
